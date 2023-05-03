6621a091190700d73fd52df781d9a7e002849b63
vishal <vishalgwaghmare31@gmail.com>
2023-05-03 14:47:04 +0530

    changes done by vishal

--- src/com/yash/Example.java
+++ src/com/yash/Example.java
@@ -5,6 +5,7 @@ public class Example {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi This Is Yash");
+		System.out.println("Hi This is Vishal");
 
 	}
 
